0a0baa76421a86ff0617ce9463261fc881b1f5af
CreamyChezy <hoadleysean@gmail.com>
2025-07-16 07:09:57 -0400

    full bowl drops worm + bowl when broken

--- src/main/java/net/sean/emporium/datagen/ModLootTableProvider.java
+++ src/main/java/net/sean/emporium/datagen/ModLootTableProvider.java
@@ -2,7 +2,12 @@ package net.sean.emporium.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.sean.emporium.block.ModBlocks;
+import net.sean.emporium.item.ModItems;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
     public ModLootTableProvider(FabricDataOutput dataOutput) {
@@ -12,6 +17,13 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
     @Override
     public void generate() {
         addDrop(ModBlocks.WORM_BLOCK);
-        addDrop(ModBlocks.PET_BOWL);
+        addDrop(ModBlocks.PET_BOWL, LootTable.builder()
+                .pool(LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModBlocks.PET_BOWL)))
+                .pool(LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(ModItems.WORM)))
+        );
     }
 }
